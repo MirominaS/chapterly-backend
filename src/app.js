2b03aad3676f9af05e21022814363790e6f66e7e
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-04-29 13:58:14 +0530

    Backend foundation: env config, routing system, middleware setup

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 import express from "express";
 import cors from "cors"
+import morgan from "morgan";
+
+import routes from "./routes/index.js"
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(morgan("dev"))
+
+app.use("/api/v1",routes)
 
 app.get("/", (req,res) => {
     res.send("Chapterly API running")
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import app from "./app.js"
+import { ENV } from "./config/env.js";
 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,() => {
-    console.log(`Server running on port ${PORT}`)
+app.listen(ENV.PORT,() => {
+    console.log(`Server running on port ${ENV.PORT}`)
 })
