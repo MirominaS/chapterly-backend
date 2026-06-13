418e1104da1116ccd901f6f0ec6b819bdfa5edaf
MirominaS <miromin-im19063@stu.kln.ac.lk>
2026-06-13 23:14:55 +0530

    feat: reorganize imports and add folder and media routes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import connectDB from "./config/db_config.js";
 import authRoutes from "./routes/authRoutes.js"
 import bookRoutes from "./routes/bookRoutes.js"
 import thoughtRoutes from "./routes/thoughtRoutes.js"
 import quoteRoutes from "./routes/quoteRoutes.js"
 import dashboardRoutes from "./routes/dashboardRoutes.js"
+import folderRoutes from "./routes/folderRoutes.js";
+import mediaRoutes from "./routes/mediaRoutes.js"
 
-dotenv.config()
+
 
 connectDB();
 
@@ -26,6 +30,9 @@ app.use("/api/thoughts",thoughtRoutes)
 app.use("/api/quotes",quoteRoutes)
 
 app.use("/api/dashboard", dashboardRoutes)
+
+app.use("/api/folders", folderRoutes);
+app.use("/api/media", mediaRoutes);
 
 const PORT = process.env.PORT || 5000
 
